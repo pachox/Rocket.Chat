b18a55302738f30377263e49f1bb697bd1baea62
pachox <p.baratta@fabbricadigitale.it>
2017-02-01 07:56:09 +0100

    Changes to be committed: 	modified:   .travis/namefiles.sh

--- .travis/namefiles.sh
+++ .travis/namefiles.sh
@@ -6,4 +6,4 @@ IFS=$'\n\t'
 FILENAME="$ROCKET_DEPLOY_DIR/rocket.chat-$ARTIFACT_NAME.tgz";
 
 ln -s /tmp/build/Rocket.Chat.tar.gz "$FILENAME"
-gpg --armor --detach-sign "$FILENAME"
+#gpg --armor --detach-sign "$FILENAME"
