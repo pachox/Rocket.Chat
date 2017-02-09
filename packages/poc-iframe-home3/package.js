fc4cd535f36544ac087df6ea3a1268b7ba8c7058
thinkingmik <michi.andreoli@gmail.com>
2017-02-09 15:52:55 +0100

    add home.js and configurable uri

--- packages/poc-iframe-home3/package.js
+++ packages/poc-iframe-home3/package.js
@@ -16,13 +16,16 @@ Package.onUse(function(api) {
 	api.use('kadira:flow-router', 'client');
 	api.use('jquery', 'client');
 	api.use('kendo-ui-pro');
+	api.use('mizzao:bootstrap-3');
 	api.use('benjaminrh:jquery-cookie', 'client');
 
-	api.addFiles('route.js', 'client');
+	api.addFiles('server/settings.js', 'server');
+	
+	api.addFiles('route.js', 'client');	
 	api.addFiles('client/addLink.js', 'client');
 	api.addFiles('client/css/styles.less', 'client');
 	api.addFiles('client/css/style.css', 'client');
+	api.addFiles('client/home.js', 'client');
 	api.addFiles('client/iframeLink.html', 'client');
-	api.addFiles('client/iframeArea.html', 'client');
-	api.addFiles('server/settings.js', 'server');
+	api.addFiles('client/iframeArea.html', 'client');	
 });
