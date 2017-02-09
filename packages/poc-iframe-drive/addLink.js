4b3619fe6727813dd7a8ab0c5f1c900146380d85
pachox <p.baratta@fabbricadigitale.it>
2017-02-09 16:09:48 +0100

    Changes to be committed: 	modified:   packages/poc-iframe-drive/addLink.js 	modified:   packages/poc-iframe-mail/addLink.js

--- packages/poc-iframe-drive/addLink.js
+++ packages/poc-iframe-drive/addLink.js
@@ -1,6 +1,6 @@
 RocketChat.roomTypes.add(null, 0, {
 	template: 'iframeLink',
-	icon: 'icon-link'
+	icon: 'icon-link',
 	condition() {
 		return RocketChat.settings.get('Drive_Enable');
 	}
--- packages/poc-iframe-mail/addLink.js
+++ packages/poc-iframe-mail/addLink.js
@@ -1,6 +1,6 @@
 RocketChat.roomTypes.add(null, 0, {
 	template: 'iframe-eMail',
-	icon: 'icon-link'
+	icon: 'icon-link',
 	condition() {
 		return RocketChat.settings.get('Mail_Enable');
 	} 
